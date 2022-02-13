10303680a62a4ba3aee1eef4f4b1286c08dd45e1
Abhishek Shah <abhishekshah3@gmail.com>
2022-02-13 17:04:18 +0530

    decoration

--- code/main.rs
+++ code/main.rs
@@ -1,52 +1,21 @@
-#![allow(unused_imports)]
-use futures::Future;
-use futures::future::{FutureExt, ready, BoxFuture, Ready, self, Map};
+use futures::future::{ready, Ready};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
-use rand::rngs::adapter::ReseedingRng;
-use serde_json::Value;
-use tokio::time::Instant;
 use std::convert::Infallible;
-use std::marker::PhantomData;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::pin::Pin;
 use std::task::{Context, Poll};
 use tower::Service;
 
 // Local imports
 mod logger;
 mod timeout;
+mod async_fns;
+mod hello_world;
+
 use timeout::Timeout;
 use logger::Logger;
-async fn shutdown_signal() {
-    // Wait for the CTRL+C signal
-    tokio::signal::ctrl_c()
-    .await
-    .expect("failed to install CTRL+C signal handler");
-    log::warn!("Shutting down");
-}
-
-async fn lazy_function(_req : Request<Body>) -> Result<Response<Body>, Infallible> {
-    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
-    Ok(
-        Response::new(
-            Body::from(
-                "Hello from LAZY function"
-            )
-        )
-    )
-}
-
-
-async fn quick_function(_req : Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(
-        Response::new(
-            Body::from(
-                "Hello from QUICK function"
-            )
-        )
-    )
-}
+use async_fns::*;
+use hello_world::HelloWorld;
 
 #[tokio::main]
 async fn main() {
@@ -80,27 +49,6 @@ async fn main() {
                         .with_graceful_shutdown(shutdown_signal());
     if let Err(e) = server.await {
         eprintln!("Server Error : {e}");
-    }
-}
-// Doesn't contain any data so clones and copies are trivial
-#[derive(Clone, Copy)]
-struct HelloWorld;
-
-// Implement for a regular HTTP request
-impl Service<Request<Body>> for HelloWorld {
-    type Response = Response<Body>;
-    type Error = Infallible;
-    // Ready is Unpin so it works with our current LoggerFuture
-    type Future = Ready<Result<Self::Response, Self::Error>>;
-    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
-    }
-    // Problem that we are trying to solve is how to implement logging in a tower service
-    // when services can be layered. Also we want to avoid allocation on every request.
-    fn call(&mut self, _req: Request<Body>) -> Self::Future {
-        ready(
-           Ok(Response::new(Body::from("Hello World from immediate future",))) 
-        )
     }
 }
 
